602bddc1508ea3d75738b3ff827badb33da6a07f
geoffrey <geoffreygosset123@gmail.com>
2022-01-16 01:47:28 +0100

    GG début Card à dupliquer avec props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import CardPresentation from "./CardPresentation/CardPresentation"
+import Card from "./Card/Card"
+
+
+
 function App() {
   return (
     <div className="App">
       <div className="englobante row w-50 mx-auto bg-danger">
       <CardPresentation />
-
+      <Card />
       </div>
     </div>
   );
---
+++ src/Card/Card.js
@@ -0,0 +1,10 @@
+
+export default function Card() {
+    return (
+        <div className="col-3">
+            <div>
+                <p>Bonjour</p>
+            </div>
+        </div>
+    )
+}
