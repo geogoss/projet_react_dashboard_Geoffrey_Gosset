cd6260352e04f8e5238ee7d86b6de3fbb23b8bbc
geoffrey <geoffreygosset123@gmail.com>
2022-01-17 16:32:55 +0100

    GG props pour un des 3 conditions presque terminés

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,18 @@
+import './Card.css'
 
-export default function Card() {
+
+export default function Card(props) {
     return (
-        <div className="col-3">
-            <div>
-                <p>Bonjour</p>
+        <div className="plan col-3 d-flex align-items-end p-0 m-1 position-relative">
+            <div className='carteSvg'>{props.svgName} </div>
+            <div className="carte m-0 w-100">
+                <div className="carteHeader d-flex justify-content-around text-center ">
+                    <p className='carteP1'>{props.txt}</p>
+                    <a className='carteA text-decoration-none' href="#">...</a>
+                </div>
+                <p className='carteP2 my-0 ps-3'>{props.hrs}</p>
+                <p className='carteP3 my-0 ps-3'>{props.txt2} </p>
+
             </div>
         </div>
     )
--- src/CardPresentation/CardPresentation.js
+++ src/CardPresentation/CardPresentation.js
@@ -3,18 +3,18 @@ import './CardPresentation.css'
 
 export default function CardPresentation() {
     return (
-        <div className='cadre mx-0 px-0 col-3 text-white'>
+        <div className='cadre mx-2 px-0 text-white'>
             <div className='presentation my-auto d-flex flex-column align-items-start ps-3 w-100'>
                 <div className="divImgJeremy">
                     <img className='imgJere' src={imgJere} alt="" />
                 </div>
                 <p className="txtSousImg pb-0 mb-0 mt-4">Report for</p>
-                <h2 className="h1Jeremy pt-0 mt-0">Jeremy <br />Robson</h2>
+                <p className="h1Jeremy pt-0 mt-0 mb-5">Jeremy <br />Robson</p>
             </div>
-            <div className='sousPresentation ps-3 my-2'>
-            <p className='my-1'>Daily</p>
-            <p className='my-1'>Weekly</p>
-            <p className='my-1'>Monthly</p>
+            <div className='sousPresentation ps-3 my-2 d-flex flex-column'>
+            <a className='my-1 text-decoration-none btnA' href="">Daily</a>
+            <a className='my-1 text-decoration-none btnA' href="">Weekly</a>
+            <a className='my-1 text-decoration-none btnA' href="">Monthly</a>
 
             </div>
         </div>
